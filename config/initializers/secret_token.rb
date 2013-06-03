ef6dbe841fd74b730549ac43d068c2cdb93b4dc0
SapoRules <asaf_lopez@outlook.com>
2013-06-03 18:47:44 -0500

    creado proyecto en Ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attom::Application.config.secret_token = '40c59de3d887b4f7630f2912e961888fd63f2a57ebd9a0bdc245b69db6cedf441bb1b97c41849cc82f4133f7c7af093dc9bb8ec67dfbf64eb3817aa2a7c64b5e'
